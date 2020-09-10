f431c52e6c5ad51d002ab8dd56feb637b1a4b279
karye <karye.webb@gmail.com>
2020-09-10 11:45:22 +0200

    Tränat på datatyper int, double

---
+++ Kapitel-2/Exempel-2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exempel_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Deklarera variabler
+            string namn = "";
+            int ålder = 0;
+            int ålderÅr2025 = 0; // camelCase
+            double längd = 1.5;
+            string meddelande = "";
+
+            // Läs in namnet
+            Console.Write("Vad heter du? ");
+            namn = Console.ReadLine();
+
+            // Läs in längd i meter
+            Console.Write("Hur lång är du? ");
+            längd = double.Parse(Console.ReadLine());
+
+            // Läs in ålder
+            Console.Write("Hur gammal är du? ");
+            ålder = int.Parse(Console.ReadLine());
+
+            // Hur gammal är användaren år 2025
+            ålderÅr2025 = ålder + 5;
+
+            // Sammanställ resultatet
+            meddelande = "Hej! " + namn + " du är " + längd + " m lång, och blir " + ålderÅr2025 + " år gammal 2025.";
+
+            Console.WriteLine(meddelande);
+        }
+    }
+}
---
+++ Kapitel-2/Uppgift-2-3/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 
+Skapa ett program som frågar användaren hur högt hen kan hoppa i höjdhopp mätt i meter. Berätta för användaren hur mycket högre världsrekordet i höjdhopp är (världsrekordet i höjdhopp är 2,45 meter).
+ */
+
+using System;
+
+namespace Uppgift_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Läs in längden
+            Console.Write("Hur högt hoppar du? ");
+            double minLängd = double.Parse(Console.ReadLine());
+
+            // Räkna ut skillnad till världsrekord
+            double skillnad = Math.Round(2.45 - minLängd, 2);
+
+            Console.WriteLine("Du är " + skillnad + " från världsrekordet i höjhopp 2,45 m.");
+        }
+    }
+}
