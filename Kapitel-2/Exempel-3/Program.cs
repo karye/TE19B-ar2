23ca19587a93067018c7d16bdca31f20a18fb839
karye <karye.webb@gmail.com>
2020-09-11 15:05:19 +0200

    Ny övning

---
+++ Kapitel-2/Exempel-3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exempel_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Läs in namnet
+            Console.Write("Hur ? ");
+            namn = Console.ReadLine();
+
+            // Läs in namnet
+            Console.Write("Vad heter du? ");
+            namn = Console.ReadLine();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
