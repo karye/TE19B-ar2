2042f39c2d3747b10aad76e6eae84fa6e65b66f6
karye <karye.webb@gmail.com>
2020-09-21 16:32:48 +0200

    Fix

--- _prog/VarSubtitution/Program.cs
+++ _prog/VarSubtitution/Program.cs
@@ -1,15 +1,11 @@
 ﻿using System;
 
-namespace Exempel_3
+namespace VarSubtitution
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Läs in namnet
-            Console.Write("Vad heter du? ");
-            string namn = Console.ReadLine();
-
             Console.WriteLine("Hello World!");
         }
     }
