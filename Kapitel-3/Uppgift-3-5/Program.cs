df65b2169939e5316aaa98df5cd78060a3a6b784
karye <karye.webb@gmail.com>
2020-09-24 10:31:49 +0200

    Övat på if-sats och boolska uttryck

---
+++ Kapitel-3/Uppgift-3-5/Program.cs
@@ -0,0 +1,28 @@
+﻿/* 
+För att få åka en viss berg-och-dalbana på en nöjespark så måste man vara mellan 1,5 och 1,9 meter lång. Skapa ett program som frågar användaren hur lång hen är och sedan skriver ut om hen får åka berg-och-dalbanan eller inte.
+ */
+
+using System;
+
+namespace Uppgift_3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Läsa in längd
+            Console.Write("Hur lång är du? ");
+            double längd = double.Parse(Console.ReadLine());
+
+            // Svara med om man får eller inte åka
+            if (1.5 < längd && längd < 1.9)
+            {
+                Console.WriteLine("Du får åka berg-och-dalbana!");
+            }
+            else
+            {
+                Console.WriteLine("Du får tyvärr inte åka!");
+            }
+        }
+    }
+}
---
+++ Kapitel-3/Uppgift-3-6/Program.cs
@@ -0,0 +1,29 @@
+﻿/* 
+Ett kafé utlyser en kampanj där personer äldre än 65 år samt personer mellan 10 och 18 erbjuds att köpa kaffe till ett extra billigt pris. Skriv ett program som innehåller endast en if-sats som undersöker om användaren kan få köpa kaffe extra billigt.
+ */
+
+using System;
+
+namespace Uppgift_3_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Läsa in en ålder
+            Console.Write("Hur lång är du? ");
+            int ålder = int.Parse(Console.ReadLine());
+
+            // Svara om man får extrapriset eller inte
+            // äldre än 65 och mellan 10 och 18 år
+            if (ålder > 65 || (ålder > 10 && ålder < 18))
+            {
+                Console.WriteLine("Du får rabatten!");
+            }
+            else
+            {
+                Console.WriteLine("Du får inte rabatten!");
+            }
+        }
+    }
+}
