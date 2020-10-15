5892867d990f8fc5745270ac57f6aafb838ef34d
karye <karye.webb@gmail.com>
2020-10-15 11:14:26 +0200

    Tränat på övningsprov

---
+++ Kapitel-4/Exempel-4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Exempel_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Slumpa fram ett tal (1-6)
+            Random tärning = new Random();
+            int slumptal = tärning.Next(1, 7);
+
+            // Deklarera variabeln gissning
+            int gissning = 0;
+
+            // Loopar tills vi får rätt svar
+            while (gissning != slumptal)
+            {
+                // Fråga anvädaren vad hen gissar
+                Console.WriteLine("Gissa ett tal (1-6)? ");
+
+                // Läs in användarens gissning
+                //gissning = int.Parse(Console.ReadLine());
+
+                // En säkrare inmattning
+                string input = Console.ReadLine();
+
+                // Försöker översätta till ett heltal
+                if (int.TryParse(input, out gissning))
+                {
+                    Console.WriteLine("Vad bra att du matade in ett tal!");
+
+                    // Kolla om gissningen är fel
+                    if (gissning != slumptal)
+                    {
+                        Console.WriteLine("Fel svar! Försök igen!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Nej! Jag bad om ett tal! Försök igen!");
+                }
+
+            }
+
+            // Rätt svar!
+            Console.WriteLine("Rätt svar!");
+        }
+    }
+}
---
+++ Kapitel-4/ÖvningsProv1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ÖvningsProv1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Deklarera variabler
+            int poäng = 0;
+            int gissning = 0;
+            string gissningText = "";
+            //bool lyckades = false;
+            int flagga = 0;
+
+            // Ställa frågan
+            Console.WriteLine("Vad är x i formeln x * 6 = 42?");
+
+            // Läs in användarens svar tills hen matar in ett tal
+            while (flagga == 0)
+            {
+                // Läs in det användaren skriver
+                gissningText = Console.ReadLine();
+
+                // Är det ett tal eller inte?
+                // Blir true eller false
+                if (int.TryParse(gissningText, out gissning))
+                {
+                    flagga = 1;
+                }
+                else
+                {
+                    // Varnar användaren om hen inte matat in ett tal
+                    Console.WriteLine("Fel! Vg mata in ett tal.");
+                }
+
+            }
+
+            // Kolla om gissningen är korrekt eller inte
+            if (gissning == 7)
+            {
+                poäng++;
+                Console.WriteLine($"Korrekt svar! Du har {poäng} poäng.");
+            }
+            else
+            {
+                Console.WriteLine("Fel svar!");
+            }
+        }
+    }
+}
