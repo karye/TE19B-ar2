9b63eb3bce2a42835728e975f299d3c53cdca335
karye <karye.webb@gmail.com>
2020-11-11 15:14:01 +0100

    Stödkommentar för uppgifter i projekt 21:an

---
+++ Kapitel-4/LyckoHjul/Program.cs
@@ -0,0 +1,26 @@
+﻿/* 
+Skapa ett program som ska fungera som ett lyckohjul. När användaren kör programmet får hen välja ett tal från 1 till 10 som hen tror att lyckohjulet kommer att stanna på. Slumpa därefter fram ett tal från 1 till 10 och säg till användaren vilket tal det blev. Berätta också för användaren om hen vann eller förlorade.
+ */
+using System;
+
+namespace LyckoHjul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Be användaren gissa ett tal 1-10
+
+
+            // Slumpa fram ett tal 1-10
+
+
+            // Berätta för användaren vilket blev slumptalet
+
+
+            // Berätta för användaren om man vann eller inte
+
+
+        }
+    }
+}
---
+++ Kapitel-4/TarningSimulator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TarningSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Fråga hur många gånger tärningen skall kastas?
+
+            // Fråga hur många sidor har varje tärning?
+
+            // Kasta tärningen ett antal gånger
+            // För varje kast skriv ut ett slumptal enligt mellan 1 och antalet sidor
+        }
+    }
+}
