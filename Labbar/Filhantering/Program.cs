258c472cd6591883fdf3f5c52a544d12b1c8504f
Karye <karye.webb@gmail.com>
2021-01-14 11:18:13 +0100

    Övat på filhantering

---
+++ Labbar/Filhantering/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Filhantering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Skriva till en textfil
+            File.WriteAllText("fil1.txt", "Nu snöar det igen! Kul!");
+
+            // Läsa innehållet av en textfil
+            string allText = File.ReadAllText("./Filhantering.csproj");
+
+            // Skriv ut innehållet
+            Console.WriteLine($"Innehållet: \n{allText}");
+
+            // Fråga användaren om filnamn
+            Console.Write("Ange filnamn: ");
+            string filnamn = Console.ReadLine();
+
+            // Kontrollera att filen finns!
+            if (File.Exists(filnamn))
+            {
+                // Läs in allt
+                string texten = File.ReadAllText(filnamn);
+                Console.WriteLine(texten);
+            } else
+            {
+                Console.WriteLine($"Hittade inte filen \"{filnamn}\"");
+            }
+        }
+    }
+}
---
+++ Labbar/Filhantering2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Filhantering2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Filnamnet
+            string filnamn = "./animals.txt";
+
+            // Läs in alla rader
+            string[] rader = File.ReadAllLines(filnamn);
+
+            // Läs in sökterm
+            Console.Write("Ange sökterm (första boktaven): ");
+            string sökterm = Console.ReadLine();
+
+            // Loopa igenom arrayen
+            // Dvs gå igenom rad-för-rad
+            int radNr = 1;
+            foreach (var rad in rader)
+            {
+                // Kolla om första bokstaven = sökterm
+                if (rad.Substring(0, 1) == sökterm)
+                {
+                    Console.WriteLine($"{radNr}\t{rad}");
+                    radNr++;
+                }
+            }
+
+            // Antal hittade rader
+            Console.WriteLine($"Antal djur {radNr - 1}");
+        }
+    }
+}
