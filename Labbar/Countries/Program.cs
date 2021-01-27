89a52a0696fe8a81e12275cae9e1c50f87118615
Karye <karye.webb@gmail.com>
2021-01-27 15:23:52 +0100

    Löser uppgiften och tränar på debugging

---
+++ Labbar/Countries/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Countries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Presentera programmet
+            Console.WriteLine("Program för att hitta länder matchande sökterm.");
+            Console.WriteLine("Program listar land och landskod.");
+
+            // Läser in alla rader i textfilen, om den finns
+            if (File.Exists("countries.csv"))
+            {
+                // Läser in alla rader
+                string[] rader = File.ReadAllLines("countries.csv");
+
+                // Ber användaren om sökterm
+                Console.Write("Ange sökterm (avsluta med return): ");
+                string sökterm = Console.ReadLine();
+
+                // Loopa igenom alla rader
+                foreach (var rad in rader)
+                {
+                    // Söka i den röda fältet
+                    //Console.WriteLine(rad);   // rad är en string
+                    string[] delar = rad.Split(',');
+
+                }
+            } else
+            {
+                Console.WriteLine("Avbryter! Filen countries.csv finns inte.");
+            }
+        }
+    }
+}
