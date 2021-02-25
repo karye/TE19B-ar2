e960325332e9f8399ad00f03c19ae6e4fbc9e58e
karye <karye.webb@gmail.com>
2021-02-25 12:05:55 +0100

    Tränat på metoder

---
+++ Kapitel-6/Exempel-1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exempel_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vi skriver text vertikalt:");
+
+            // Den första texten
+            SkrivVertikalt("Metoder!!!");
+
+            // Den andra texten
+            SkrivVertikalt("Metoder är kul");
+
+            // Den tredje texten
+            SkrivVertikalt("Mycket användbart i min programmering");
+        }
+
+        // En metod för att skriva text vertikalt
+        // Parameter: string text1
+        static void SkrivVertikalt(string text1) {
+            for (int i = 0; i < text1.Length; i++)
+            {
+                Console.WriteLine(text1[i]);
+            }
+        }
+    }
+}
---
+++ Kapitel-6/Uppgift-6-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Uppgift_6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TalIKvadrat(3);
+            TalIKvadrat(4);
+            TalIKvadrat(5);
+        }
+
+        // Metod för att skriva tal och tal i kvadrat
+        static void TalIKvadrat(int tal) {
+            Console.WriteLine($"Talet {tal} i kvadrat är {tal * tal}");
+        }
+    }
+}
