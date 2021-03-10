bbaaa9d6fb8794d7a2ce44abebaed0d67508e79d
karye <karye.webb@gmail.com>
2021-03-10 15:27:32 +0100

    Tränat på dela upp i metoder

---
+++ Kapitel-6/Exempel-2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Exempel_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string svar = "";
+            while (svar != "3")
+            {
+                VisaMeny();
+                svar = Console.ReadLine();
+
+                switch (svar)
+                {
+                    case "1":
+                    SkrivUtVertikalt();
+                    break;
+
+                    case "2":
+                    SkrivUtOmvänt();
+                    break;
+
+                    case "3":
+                    //
+                    break;
+                    default:
+                }
+            }
+        }
+
+        static void VisaMeny() {
+            Console.WriteLine("Välj ett alterniv");
+            Console.WriteLine("1. Skriv ut texten vertikalt");
+            Console.WriteLine("2. Skriv ut texten omvänt");
+            Console.WriteLine("3. Avsluta");
+        }
+
+        static void SkrivUtVertikalt() {
+
+        }
+
+        static void SkrivUtOmvänt() {
+            
+        }
+    }
+}
---
+++ Kapitel-6/Max/Program.cs
@@ -0,0 +1,21 @@
+﻿﻿using System;
+
+namespace Uppgift6._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur stor ska rätvinklig triangeln vara?");
+            int sidLängd = int.Parse(Console.ReadLine());
+            RitaRätvinkligTriangel(sidLängd);
+        }
+        static void RitaRätvinkligTriangel(int längd)
+        {
+            for (int i = 0; i > längd; i++)
+            {
+                System.Console.WriteLine("*");
+            }
+        }
+    }
+}
