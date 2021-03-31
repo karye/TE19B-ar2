a78b48f69ef5d1dc2ead625c40cb4f25f837aeb0
Karye <karye.webb@gmail.com>
2021-03-31 15:01:19 +0200

    Tränat på överlagrade metoder

---
+++ Kapitel-6/Exempel-5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exempel_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Använder den första metoden
+            SkrivVertikalt("Hej");
+
+            // Testa den andra metoden
+            SkrivVertikalt(12);
+        }
+
+        static void SkrivVertikalt(string meddelande)
+        {
+            // Loopa igenom texten tecken för tecken
+            for (int i = 0; i < meddelande.Length; i++)
+            {
+                Console.WriteLine(meddelande[i]);
+            }
+        }
+
+        static void SkrivVertikalt(int tal)
+        {
+            SkrivVertikalt(tal.ToString());
+        }
+    }
+}
